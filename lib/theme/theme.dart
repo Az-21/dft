ea9ce0dd8e8f851d7b28bf6fa2b345660d0752d9
Az-21 <39803478+Az-21@users.noreply.github.com>
2022-05-25 00:09:40 +0530

    🧼 remove mui2 theme colors

--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -5,19 +5,11 @@ import 'package:flutter/material.dart';
 class Themes {
   final darkTheme = ThemeData.dark().copyWith(
     useMaterial3: true,
-    primaryColor: Colors.green,
-    appBarTheme: const AppBarTheme(
-      color: Colors.green,
-    ),
     brightness: Brightness.dark,
   );
 
   final lightTheme = ThemeData.light().copyWith(
     useMaterial3: true,
-    primaryColor: Colors.green,
-    appBarTheme: const AppBarTheme(
-      color: Colors.green,
-    ),
     brightness: Brightness.light,
   );
 }
