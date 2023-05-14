f79d4dbb8f00007a12de161bad1dc543e385d93b
Az-21 <39803478+Az-21@users.noreply.github.com>
2023-05-14 11:29:33 +0530

    Implement adaptive material theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:dft/src/about.dart';
 import 'package:dft/src/home.dart';
-import 'package:dft/theme/theme.dart';
 import 'package:dft/theme/theme_service.dart';
+import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
@@ -11,15 +11,39 @@ void main() async {
   runApp(MyApp());
 }
 
+const _fallbackGreen = Color(0xFF386A20);
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      title: 'DFT Calculator',
-      theme: Themes().lightTheme,
-      darkTheme: Themes().darkTheme,
-      themeMode: ThemeService().getThemeMode(),
-      home: const DarkModeHome(),
+    return DynamicColorBuilder(
+      builder: (ColorScheme? lightDynamic, ColorScheme? darkDynamic) {
+        ColorScheme lightColorScheme;
+        ColorScheme darkColorScheme;
+
+        if (lightDynamic != null && darkDynamic != null) {
+          // On Android S+ devices, use the provided dynamic color scheme.
+          lightColorScheme = lightDynamic.harmonized();
+          darkColorScheme = darkDynamic.harmonized();
+        } else {
+          // Otherwise, use fallback schemes.
+          lightColorScheme = ColorScheme.fromSeed(
+            seedColor: _fallbackGreen,
+          );
+          darkColorScheme = ColorScheme.fromSeed(
+            seedColor: _fallbackGreen,
+            brightness: Brightness.dark,
+          );
+        }
+
+        return GetMaterialApp(
+          title: 'DFT Calculator',
+          theme: ThemeData(colorScheme: lightColorScheme),
+          darkTheme: ThemeData(colorScheme: darkColorScheme),
+          themeMode: ThemeService().getThemeMode(),
+          home: const DarkModeHome(),
+        );
+      },
     );
   }
 }
