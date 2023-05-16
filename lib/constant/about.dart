b1a8bda8976a604e424bfa44394ad2d67e3b17a4
Az-21 <39803478+Az-21@users.noreply.github.com>
2023-05-16 17:45:26 +0530

    Refactor 'About' page using `.builder()` wherever possible

---
+++ lib/constant/about.dart
@@ -0,0 +1,57 @@
+const String _github = "https://github.com/";
+
+const List<List<String>> constDataInfo = [
+  [
+    "Discrete Fourier Transform Calculator",
+    "Radix2 DIT FFT Algorithm",
+    "https://en.wikipedia.org/wiki/Cooley%E2%80%93Tukey_FFT_algorithm",
+  ],
+  [
+    "Made by Az-21",
+    "Built with Flutter",
+    "${_github}Az-21",
+  ],
+  [
+    "Source Code",
+    "View on GitHub | GPL v3",
+    "${_github}Az-21/dft",
+  ],
+  [
+    "Report Issue",
+    "Open a GitHub Issue",
+    "${_github}Az-21/dft/issues",
+  ]
+];
+
+const List<List<String>> constDataCredit = [
+  [
+    "Complex",
+    "Apache 2.0 License",
+    "${_github}rwl/complex",
+  ],
+  [
+    "Dynamic Color",
+    "Apache 2.0 License",
+    "${_github}material-foundation/flutter-packages/tree/main/packages/dynamic_color",
+  ],
+  [
+    "Syncfusion Flutter Charts",
+    "Syncfusion Community License",
+    "${_github}syncfusion/flutter-widgets/tree/master/packages/syncfusion_flutter_charts",
+  ],
+  [
+    "GetX",
+    "MIT License",
+    "${_github}jonataslaw/getx",
+  ],
+  [
+    "URL Launcher",
+    "BSD 3-Clause License",
+    "${_github}flutter/packages/tree/main/packages/url_launcher/url_launcher",
+  ],
+  [
+    "Flutter Launcher Icons",
+    "MIT License",
+    "${_github}fluttercommunity/flutter_launcher_icons",
+  ],
+];
