c448a818f1efe23b927164682cd9d5aea1154c79
Az-21 <39803478+Az-21@users.noreply.github.com>
2023-05-20 11:41:55 +0530

    Update route to accept `List<Complex>` as `extra` param

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,10 +1,10 @@
+import 'package:complex/complex.dart';
+import 'package:dft/src/about.dart';
 import 'package:dft/src/home.dart';
 import 'package:dft/src/result/dftResult.dart';
 import 'package:dft/src/result/idftResult.dart';
 import 'package:dft/src/result/radix2Result.dart';
-import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:dft/src/about.dart';
 
 final router = GoRouter(routes: [
   GoRoute(path: "/", builder: (context, state) => const HomeScreen(), routes: [
@@ -13,24 +13,23 @@ final router = GoRouter(routes: [
       builder: (context, state) => const AboutPage(),
     ),
     GoRoute(
-      path: "Radix2FFT",
+      path: "FFT",
       builder: (context, state) {
-        // TODO: Add a class to encapsulate points as Complex instead of a nested list
-        List<List<TextEditingController>> points = state.extra as List<List<TextEditingController>>;
+        List<Complex> points = state.extra as List<Complex>;
         return Radix2FFT(points: points);
       },
     ),
     GoRoute(
       path: "DFT",
       builder: (context, state) {
-        List<List<TextEditingController>> points = state.extra as List<List<TextEditingController>>;
+        List<Complex> points = state.extra as List<Complex>;
         return DFT(points: points);
       },
     ),
     GoRoute(
       path: "IDFT",
       builder: (context, state) {
-        List<List<TextEditingController>> points = state.extra as List<List<TextEditingController>>;
+        List<Complex> points = state.extra as List<Complex>;
         return IDFT(points: points);
       },
     ),
