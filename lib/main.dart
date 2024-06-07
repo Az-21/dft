3f4a02f651fad13318ac83fa12a1523c4e2c3694
Az-21 <39803478+Az-21@users.noreply.github.com>
2024-06-07 09:53:28 +0530

    Re-activate predictive back gesture

--- lib/main.dart
+++ lib/main.dart
@@ -44,16 +44,16 @@ class MyApp extends StatelessWidget {
           theme: ThemeData(
             colorScheme: lightColorScheme,
             useMaterial3: true,
-            // pageTransitionsTheme: const PageTransitionsTheme(
-              // builders: { TargetPlatform.android: PredictiveBackPageTransitionsBuilder() },
-            // ),
+            pageTransitionsTheme: const PageTransitionsTheme(
+              builders: { TargetPlatform.android: PredictiveBackPageTransitionsBuilder() },
+            ),
           ),
           darkTheme: ThemeData(
             colorScheme: darkColorScheme,
             useMaterial3: true,
-            // pageTransitionsTheme: const PageTransitionsTheme(
-              // builders: { TargetPlatform.android: PredictiveBackPageTransitionsBuilder() },
-            // ),
+            pageTransitionsTheme: const PageTransitionsTheme(
+              builders: { TargetPlatform.android: PredictiveBackPageTransitionsBuilder() },
+            ),
           ),
           themeMode: EasyDynamicTheme.of(context).themeMode,
           routerConfig: router,
